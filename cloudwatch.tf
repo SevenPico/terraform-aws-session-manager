946416647a98d1886e1a5fdd1d2a6261fe20000e
Adarsh Hiwrale <ahiwrale@sigmasolve.net>
2024-05-03 01:10:16 +0530

    kms key arn replace with key id

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -2,7 +2,7 @@ resource "aws_cloudwatch_log_group" "session_manager_log_group" {
   count             = module.context.enabled ? 1 : 0
   name              = "/aws/ssm/${module.context.id}"
   retention_in_days = var.cloudwatch_logs_retention
-  kms_key_id        = module.kms_key.key_id
+  kms_key_id        = module.kms_key.key_arn
 
   tags = module.context.tags
 }
